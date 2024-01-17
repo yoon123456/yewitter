9680588b777a9039c8203b4ee81bb1a86b090ae1
yoon123456 <yw01124@gmail.com>
2024-01-17 09:24:57 +0900

    router 셋팅, firebase 셋팅

---
+++ src/component/loading-screen.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Text = styled.span`
+  font-size: 24px;
+`;
+
+const LoadingScreen = () => {
+  return (
+    <Wrapper>
+      <Text>Loading...</Text>
+    </Wrapper>
+  );
+};
+
+export default LoadingScreen;
---
+++ src/firebase.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxt2IkjAnrUOtV7WgOi08QzHrElgO53oY",
+  authDomain: "yewitter-57cee.firebaseapp.com",
+  projectId: "yewitter-57cee",
+  storageBucket: "yewitter-57cee.appspot.com",
+  messagingSenderId: "454944869454",
+  appId: "1:454944869454:web:fec4d08b1dab55a7c8c71f",
+};
+
+const app = initializeApp(firebaseConfig);
